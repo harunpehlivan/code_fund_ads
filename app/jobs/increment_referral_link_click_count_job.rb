7a7cf7da50f8fe24c966c0f313f9ee570d0d595d
Nathan Hopkins <natehop@gmail.com>
2019-02-13 16:20:52 -0700

    Get incrementor working

--- app/jobs/increment_referral_link_click_count_job.rb
+++ app/jobs/increment_referral_link_click_count_job.rb
@@ -1,8 +1,8 @@
 class IncrementReferralLinkClickCountJob < ApplicationJob
   queue_as :low
 
-  def perform(referral_code, ad)
+  def perform(referral_code)
     user = User.find_by(referral_code: referral_code)
-    user&.increment(:referral_click_count)
+    user&.increment! :referral_click_count
   end
 end
